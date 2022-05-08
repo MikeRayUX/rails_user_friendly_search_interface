f9e9270b41d750fb0ca92f392e77835d878982c1
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2022-05-07 20:18:50 -0700

    add better styles to search field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_08_024559) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_08_025928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_08_024559) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name"
   end
 
 end
