b99c3d0268188da561fbea9698b1f8860657ffad
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2022-05-10 16:14:40 -0700

    add a model spec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not" do
+    @user = User.new(name: nil)
+    expect(@user.save).to eq false
+  end
 end
