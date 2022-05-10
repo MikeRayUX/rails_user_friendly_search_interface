cd1f42eb2720ffc9266b166b38505dc931808363
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2022-05-10 15:21:10 -0700

    add a bin/check

--- app/controllers/api/v1/autocompletes_controller.rb
+++ app/controllers/api/v1/autocompletes_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::AutocompletesController < Api::ApiController
-  # api_v1_autocompletes_path | /api/v1/autocompletes GET 
+  # api_v1_autocompletes_path | /api/v1/autocompletes GET
   def index
     render json: {
       code: 200,
-      message: 'success',
+      message: "success",
       users: User.ransack(name_cont: params[:q]).result(distinct: true).limit(7).select(:id, :name)
     }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,4 @@ Rails.application.routes.draw do
       resources :autocompletes, only: :index
     end
   end
-
 end
