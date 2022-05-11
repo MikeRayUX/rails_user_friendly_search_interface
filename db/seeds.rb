2192675bfdeddae00a4d050a9cd501d32fafd066
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2022-05-10 19:57:21 -0700

    restore boilerplate seeds.rb comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
-100.times { |num| User.create(name: Faker::Name.first_name) }
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+100.times do |num|
+  User.create(name: Faker::Name.first_name)
+end
 
 p "#{User.count} users created!"
