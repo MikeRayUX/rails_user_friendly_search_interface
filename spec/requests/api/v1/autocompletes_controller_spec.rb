2e7292485a0f389e7dd2e90b23e2c5bb5e5f8ace
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2022-05-10 18:37:02 -0700

    fix broken spec

--- spec/requests/api/v1/autocompletes_controller_spec.rb
+++ spec/requests/api/v1/autocompletes_controller_spec.rb
@@ -1,51 +1,36 @@
-require "rails_helper"
-RSpec.describe "api/v1/autocompletes_controller", type: :request do
-  before do
-    7.times do |num|
-      create(:user)
-    end
-  end
+require 'rails_helper'
+RSpec.describe 'api/v1/autocompletes_controller', type: :request do
+  before { 7.times { |num| create(:user) } }
 
-  after do
-  end
-  scenario "should get index" do
-    get api_v1_autocompletes_path, params: {
-      q: nil
-    }
+  after {  }
+  scenario 'should get index' do
+    get api_v1_autocompletes_path, params: { q: nil }
 
     json = JSON.parse(response.body).with_indifferent_access
 
     expect(json[:code]).to eq(200)
-    expect(json[:message]).to eq("success")
+    expect(json[:message]).to eq('success')
   end
 
-  scenario "should get some users back with no query search param" do
-    get api_v1_autocompletes_path, params: {
-      q: nil
-    }
+  scenario 'should get some users back with no query search param' do
+    get api_v1_autocompletes_path, params: { q: nil }
 
     json = JSON.parse(response.body).with_indifferent_access
 
     expect(json[:users].length).to eq 7
   end
 
-  scenario "should get an exact match based on query string" do
-    @match_name = "michael"
+  scenario 'should get an exact match based on query string' do
+    @match_name = 'michael'
     User.create!(name: @match_name)
 
-    get api_v1_autocompletes_path, params: {
-      q: nil
-    }
+    get api_v1_autocompletes_path, params: { q: @match_name }
 
     json = JSON.parse(response.body).with_indifferent_access
 
     @matches = false
 
-    json[:users].each do |user|
-      if user[:name] == @match_name
-        @matches = true
-      end
-    end
+    json[:users].each { |user| @matches = true if user[:name] == @match_name }
 
     expect(@matches).to eq true
   end
