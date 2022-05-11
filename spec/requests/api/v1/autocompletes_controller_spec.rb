d58886fc3d346e88741c962c1d241532824eb060
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2022-05-10 18:40:07 -0700

    clean up unused after block

--- spec/requests/api/v1/autocompletes_controller_spec.rb
+++ spec/requests/api/v1/autocompletes_controller_spec.rb
@@ -1,30 +1,29 @@
-require 'rails_helper'
-RSpec.describe 'api/v1/autocompletes_controller', type: :request do
+require "rails_helper"
+RSpec.describe "api/v1/autocompletes_controller", type: :request do
   before { 7.times { |num| create(:user) } }
 
-  after {  }
-  scenario 'should get index' do
-    get api_v1_autocompletes_path, params: { q: nil }
+  scenario "should get index" do
+    get api_v1_autocompletes_path, params: {q: nil}
 
     json = JSON.parse(response.body).with_indifferent_access
 
     expect(json[:code]).to eq(200)
-    expect(json[:message]).to eq('success')
+    expect(json[:message]).to eq("success")
   end
 
-  scenario 'should get some users back with no query search param' do
-    get api_v1_autocompletes_path, params: { q: nil }
+  scenario "should get some users back with no query search param" do
+    get api_v1_autocompletes_path, params: {q: nil}
 
     json = JSON.parse(response.body).with_indifferent_access
 
     expect(json[:users].length).to eq 7
   end
 
-  scenario 'should get an exact match based on query string' do
-    @match_name = 'michael'
+  scenario "should get an exact match based on query string" do
+    @match_name = "michael"
     User.create!(name: @match_name)
 
-    get api_v1_autocompletes_path, params: { q: @match_name }
+    get api_v1_autocompletes_path, params: {q: @match_name}
 
     json = JSON.parse(response.body).with_indifferent_access
 
